093b71479146f30e63f4ce399da4356fa090b5ee
sofii554 <144987548+sofii554@users.noreply.github.com>
2024-09-11 07:13:55 +0700

    Add files via upload

---
+++ bab4/soal_1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas.bab4;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class soal_1 {
+     public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 3;
+        int c = a+b;
+        System.out.println(c);
+      
+        int k = 10;
+        int l = 3;
+        int m = a-b;
+        System.out.println(m);
+        
+        int s = 15;
+        int i = 2;
+        int z = a*b;
+        System.out.println(z);
+        
+        int j = 10;
+        int f = 3;
+        int r = a/b;
+        System.out.println(r);
+    }
+}
---
+++ bab4/soal_2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas.bab4;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class soal_2 {
+     public static void main(String[] args) {
+        // TODO code application logic here
+        int w, x, y, z;
+        w = 5;
+        x = 6;
+        y = 8-x++;
+        z = 8-++w;
+        
+        System.out.println(w);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+    }
+}
