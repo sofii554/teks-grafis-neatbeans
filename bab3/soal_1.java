2f81fa5e1c1d1c0ad69d4f0c13c04ce62acaca6a
sofii554 <144987548+sofii554@users.noreply.github.com>
2024-09-11 07:13:21 +0700

    Add files via upload

---
+++ bab3/soal_1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas.bab3;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class soal_1 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       int number = 9;
+       float angka = 8.99f;
+       double huruf = 'A';
+       boolean var4 = false;
+       String kata = "Hello World";
+       
+       System.out.println(number);
+              System.out.println(angka);
+              System.out.println(huruf);
+              System.out.println(var4);
+       System.out.println(kata);
+
+
+    }
+}
---
+++ bab3/soal_2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas.bab3;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class soal_2 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int NIM = 2110117000;
+        String Nama = "Andika Jaya Utama";
+        String kelas = "01SIFE020";
+        double Usia = 19.5;
+        double TinggiBadan = 17.2;
+        
+        System.out.println("NIM : "+NIM);
+        System.out.println("Nama Mahasiswa : "+Nama);
+        System.out.println("Kelas : "+kelas);
+        System.out.println("Usia : "+Usia);
+        System.out.println("Tinggi : "+TinggiBadan);
+
+    }
+}
