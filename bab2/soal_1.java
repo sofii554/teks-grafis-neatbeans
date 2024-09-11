cfc9e8b1168d93dcd936d2044d2f8430cab64d79
sofii554 <144987548+sofii554@users.noreply.github.com>
2024-09-11 07:12:36 +0700

    Add files via upload

---
+++ bab2/soal_1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas.bab2;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class soal_1 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 45;
+        int b = 5;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
---
+++ bab2/soal_2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas.bab2;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class soal_2 {
+     public static void main(String[] args) {
+        // TODO code application logic here
+        String makanan="Ayam Geprek";
+        String minuman="Jus jambu";
+        
+        System.out.println("Makanan Favorit Saya : " + makanan);
+        System.out.println("Minuman Favorit Saya : " + minuman);
+
+    }
+}
